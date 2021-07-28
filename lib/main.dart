30ba5ee345c18f26e6d9e07119e1c478c08e0020
David <dcobbina@bridgeinnovation.io>
2021-07-28 08:06:09 +0000

    deleted unused files and folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:aerium/app_theme.dart';
 import 'package:aerium/presentation/pages/home/home_page.dart';
 import 'package:aerium/presentation/routes/routes.dart';
 import 'package:aerium/values/values.dart';
 
-import 'injection_container.dart' as di;
-import 'application/simple_bloc_delegate.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await di.init();
-  BlocSupervisor.delegate = SimpleBlocDelegate();
-  GoogleFonts.config.allowRuntimeFetching = false;
-  runApp(MyApp());
+  // GoogleFonts.config.allowRuntimeFetching = false;
+  runApp(Aerium());
 }
 
-class MyApp extends StatelessWidget {
+class Aerium extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:aerium/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Aerium());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
