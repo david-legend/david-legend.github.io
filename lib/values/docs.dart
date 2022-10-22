acfb31fa89c63ae70c7bac9f8f0ed49803f356fe
David Cobbina <davidcobbina47@gmail.com>
2022-10-22 15:15:55 +0000

    updated cv route

--- lib/values/docs.dart
+++ lib/values/docs.dart
@@ -2,7 +2,7 @@ part of values;
 
 class DocumentPath {
   //Docs route
-  static const String docDir = "assets/images";
+  static const String docDir = "assets/assets/images";
   // static const String docDir = "assets/documents";
 
   //Docs
